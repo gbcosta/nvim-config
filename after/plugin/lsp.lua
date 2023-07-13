1a018f7fa26282ff9615a255053c1a44f9647e4d
gbcostasant@gmail.com <gbcostasant@gmail.com>
2023-07-13 14:08:14 -0300

    add some keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,3 +13,17 @@ lsp.set_preferences({
 lsp.setup_servers({'tsserver', 'eslint'})
 
 lsp.setup()
+
+-- You need to setup `cmp` after lsp-zero
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }
+})
